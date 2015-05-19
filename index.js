7c17719b09df5556c5c7100bac1e1bef1e7fcdaa
Yusuf Erdogan <erdoganyusuf@gmail.com>
2015-05-19 21:43:56 +0100

    improving

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
+
+vat request = function(request, response) {
 	console.log(request.headers);
   response.send('{ "text": "heyo" }');
-});
+};
+
+
+app.get('/', request);
+app.post('/', request);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running on port:" + app.get('port'))
