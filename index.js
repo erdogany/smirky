88f1a3e1b1d87ca576b1f807e302bbfd8067f232
Yusuf Erdogan <erdoganyusuf@gmail.com>
2015-05-21 23:02:23 +0100

    improving

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
+var GoogleSearch = require('google-search');
+
+
+var googleSearch = new GoogleSearch({
+  key: 'AIzaSyAGGGZi0KlrmiBRKryMICgrz16hcsxyLEI',
+  cx: '016010965254068421165:dvys1o6nylk'
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -8,9 +15,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 var request = function(req, res) {	
-	console.log(req.body.text);
-  res.send('{ "text": "@' + req.body.text + '" }');
+	console.log(req.body.text);	
+	googleSearch.build({
+	  q: req.body.text,
+	  start: 5,
+	  fileType: "pdf",
+	  gl: "uk", //geolocation, 
+	  lr: "lang_en",
+	  num: 10, // Number of search results to return between 1 and 10, inclusive 
+	  siteSearch: "http://kitaplar.ankara.edu.tr/" // Restricts results to URLs from a specified site 
+	}, function(error, response) {
+	  if(error) {
+	  	res.send('{ "text": "error" }');
+	  } else {
+	  	console.log(response);
+	  	res.send('{ "text": "success" }');
+	  }	  
+	});
+  
 };
+
+
+
+
 
 
 app.get('/', request);
